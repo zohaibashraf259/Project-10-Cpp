10e6206108779d3f2159fa41db84c605eb51d1f3
Zohaib Ashraf <zohaibashraf2559@gmail.com>
2025-08-05 11:18:20 +0500

    Add files via upload

---
+++ Proj10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int num1,num2, Result;
+
+    cout << "Enter your First number: ";
+    cin >> num1;
+
+    cout <<"Enter your second number: ";
+    cin >> num2;
+
+    Result = num1 * num2 ;
+    cout << "Result of multiplication is: " << Result <<endl;
+
+    return 0;
+}
